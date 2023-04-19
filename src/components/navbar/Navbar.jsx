1f355a53b8b56fb1cd1383f7215b3afa561d5fb1
unknown <JGM@NJ0657.jgm.ar>
2023-04-19 16:03:19 -0300

    inicio

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    
+        <div className='ListaNav'> 
+        <a href="#">Logo</a>
+        <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Products</a></li>
+        <li><a href="#">Contact</a></li>
+        <li><a href="#">About Us</a></li>
+        </ul> 
+        
+        </div>
+    
+  )
+}
